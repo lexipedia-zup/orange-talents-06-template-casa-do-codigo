a4dbe9f17b4f4b805f30765d526a3f8c6840b2c1
lexipediaa <alexsander.viana@zup.com.br>
2021-07-05 10:40:43 -0300

    Alteração de interface utilizada
     - Feito troca do uso da interface CrudRepository para JpaRepository, visto que à longo prazo pode ser mais eficaz.

--- src/main/java/br/com/zup/academy/casadocodigo/autor/AutorRepository.java
+++ src/main/java/br/com/zup/academy/casadocodigo/autor/AutorRepository.java
@@ -1,10 +1,10 @@
 package br.com.zup.academy.casadocodigo.autor;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AutorRepository extends CrudRepository<Autor, Integer> {
+public interface AutorRepository extends JpaRepository<Autor, Integer> {
 
     Optional<Autor> findByEmail(String email);
 
